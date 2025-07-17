4ca6c334d8ca30b46a10dc469dcc7c722c3679a9
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-17 19:26:21 +0530

    f1

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'home_page.dart';
 
 class LoginPage extends StatelessWidget {
   final emailCtrl = TextEditingController();
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
-import 'admin_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
