b71c0d44c920cf80a4e5a4547efde102d112d5ac
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 12:44:20 +0530

    error solving

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,54 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
 
-  // Firebase.initializeApp(
-  //     options: FirebaseOptions(
-  //         apiKey: "AIzaSyA8fzfImo8PgTyup_NI-gkwLuPIKD_NdEk",
-  //         authDomain: "bus-system-641e0.firebaseapp.com",
-  //         projectId: "bus-system-641e0",
-  //         storageBucket: "bus-system-641e0.firebasestorage.app",
-  //         messagingSenderId: "702515887588",
-  //         appId: "1:702515887588:web:fc42a25a7e9634001e1bf1"));
-  runApp(MaterialApp(
-    home: LoginPage(),
-    debugShowCheckedModeBanner: false,
-    //home: AdminPage(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bus Attendance App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: AuthWrapper(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        
+        if (snapshot.hasData) {
+          // User is logged in
+          return HomePage();
+        } else {
+          // User is not logged in
+          return LoginPage();
+        }
+      },
+    );
+  }
 }
